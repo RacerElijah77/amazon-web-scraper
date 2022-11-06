4e25122c1b139d2c77be5f65205d99c68a135962
Elijah Gasataya <gasatae1@tcnj.edu>
2022-11-06 18:21:50 -0500

    Added comment to main code

--- amazon_results_scraper.py
+++ amazon_results_scraper.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+'''
+    CSC 360 - Project 2 (Python)
+    EJ Gasataya
+    Parvathi Krishnan
+    Michael Fecht
+    Computer Networking Fall 2022 - Dr. Li
+    Python Web Scraping Amazon reviews and profiles
+'''
 
 
 from selenium import webdriver
